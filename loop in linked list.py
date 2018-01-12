3f81005542bf1aca419d22f2c91b754cbc87bee2
risha97 <rishavanand1997@gmail.com>
2018-01-12 23:42:38 +0530

    Code 4 Loop in linked list

---
+++ loop in linked list.py
@@ -0,0 +1,22 @@
+"""
+Detect a cycle in a linked list. Note that the head pointer may be 'None' if the list is empty.
+
+A Node is defined as:
+
+    class Node(object):
+        def __init__(self, data = None, next_node = None):
+            self.data = data
+            self.next = next_node
+"""
+
+
+def has_cycle(head):
+    fast = head
+    while (fast != None and fast.next != None):
+        fast = fast.next.next
+        head = head.next
+
+        if head == fast:
+            return 1
+
+        return 0

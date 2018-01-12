b0b4444a0f2035536ed3b5623858cb25668596d0
risha97 <rishavanand1997@gmail.com>
2018-01-12 23:06:05 +0530

    Code 2 Bubble swaps

---
+++ Swaps count.py
@@ -0,0 +1,15 @@
+#https://www.hackerrank.com/challenges/ctci-bubble-sort/problem
+
+n = int(input().strip())
+a = list(map(int, input().strip().split(' ')))
+for i in range(n):
+    for j in range(n - 1):
+        count = 0
+        if a[j] > a[j + 1]:
+            a[j], a[j + 1] = a[j + 1], a[j]
+            count = count + 1
+
+print("Array is sorted in", count, "swaps.")
+print("First Element:", a[0])
+print("Last Element:", a[n - 1])
+
